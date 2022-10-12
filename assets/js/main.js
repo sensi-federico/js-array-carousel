561c666087ad371fe2bb755a9c67d79468d4f252
sensiz <dev.federicosensi@gmail.com>
2022-10-12 17:13:12 +0200

    next button complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 // Consegna:
 // Dato un array contenente una lista di cinque immagini, creare 
 // un carosello come nello screenshot allegato.
-    
+
 // MILESTONE 1
 // Per prima cosa, creiamo il markup statico: 
 // costruiamo il container e inseriamo un'immagine grande al centro: 
@@ -13,10 +13,47 @@
 
 // array contenente immagini
 
-const slide = [
+const slides = [
     './assets/img/01.webp',
     './assets/img/02.webp',
     './assets/img/03.webp',
     './assets/img/04.webp',
     './assets/img/05.webp'
 ];
+
+// MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo tutte 
+// le immagini dinamicamente servendoci 
+// dell'array fornito e un semplice ciclo for che concatena 
+// un template literal. 
+// Tutte le immagini saranno nascoste, tranne la prima, 
+// che avrà una classe specifica che la renderà visibile. 
+// Al termine di questa fase ci ritroveremo con lo stesso slider 
+// stilato nella milestone 1, ma costruito
+// dinamicamente attraverso JavaScript.
+
+const slidesEl = document.querySelector('div.slides');
+
+let imageActive = 0;
+
+for (let i = 0; i < slides.length; i++) {
+    const slideUrl = slides[i];
+    const markUp = `<img class="${i === imageActive ? 'active' : ''}" src="${slideUrl}" alt="">`
+    slidesEl.insertAdjacentHTML('beforeend', markUp);
+}
+
+const nextButtonEl = document.querySelector('.next');
+const prevButtonEl = document.querySelector('.prev');
+
+nextButtonEl.addEventListener('click', function() {
+    const images = document.querySelectorAll('.slides > img');
+    const currentSlide = images[imageActive];
+
+    currentSlide.classList.remove('active');
+    imageActive++;
+    
+    const nextSlide = images[imageActive];
+    nextSlide.classList.add('active');
+})
+
+ 
