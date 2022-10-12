2592152164c2e4833abf2948aa6515eb2c6ba773
sensiz <dev.federicosensi@gmail.com>
2022-10-12 16:24:43 +0200

    main structure commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+
+// Consegna:
+// Dato un array contenente una lista di cinque immagini, creare 
+// un carosello come nello screenshot allegato.
+    
+// MILESTONE 1
+// Per prima cosa, creiamo il markup statico: 
+// costruiamo il container e inseriamo un'immagine grande al centro: 
+// avremo così la struttura base e gli stili pronti per poterci poi 
+// concentrare solamente sull'aspetto logico.
+
+
+// array contenente immagini
+
+const slide = [
+    './assets/img/01.webp',
+    './assets/img/02.webp',
+    './assets/img/03.webp',
+    './assets/img/04.webp',
+    './assets/img/05.webp'
+];
